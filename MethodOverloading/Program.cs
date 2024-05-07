fd0439b378d8e270f3fda8152bd71a32787bba43
jessvand <jmvandbeek@gmail.com>
2024-05-07 13:54:25 -0400

    completed overloading w/ decimals

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace MethodOverloading
+﻿using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var intAdd = Add(2, 6);
+            Console.WriteLine(intAdd);
+
+            var decimalAdd = Add(5.0m, 3.0m);
+
+            var addWithBool = Add(1, 0, true);
+            Console.WriteLine(addWithBool);
         }
+
+        public static int Add(int numOne , int numTwo) 
+        {
+            return numOne + numTwo;
+        }
+        
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool isChecked)
+        {
+            var sum = numOne + numTwo;
+
+            if (isChecked && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if(isChecked && sum ==1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+
+
     }
 }
